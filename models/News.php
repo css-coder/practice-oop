d3ecb6f8d35493980ec02d126664e98f04d886c1
css-coder <moskitiks@gmail.com>
2018-01-07 16:04:03 +0300

    Добавлен класс для модели News

---
+++ models/News.php
@@ -0,0 +1,21 @@
+<?php
+    class News
+    {
+        /**
+         *  Получаем полную новость по id
+         * @param integer $id
+         */
+        public static function getNewsItemById($id)
+        {
+            // Запрос к бд
+        }
+
+        /**
+         *  Получаем список новостей
+         * @param array
+         */
+        public static function getNewsList()
+        {
+            // Запрос к бд
+        }
+    }
