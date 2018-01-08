0b316ba28e7ba1b2c65b4a06b57f7143d3ea5d58
css-coder <moskitiks@gmail.com>
2018-01-08 12:39:54 +0300

    Измененены экшены для чпу

--- config/routes.php
+++ config/routes.php
@@ -2,10 +2,6 @@
 
     return array(
 
-        'news/([a-z]+)/([0-9]+)' => 'news/view/$1/$2', // actionView в NewsController
-
-        //'news/([0-9]+)' => 'news/view',  // actionView в NewsController
+        'news/([0-9]+)' => 'news/view/$1',  // actionView в NewsController
         'news' => 'news/index',  // actionIndex в NewsController
-        'articles' => 'article/index',  // actionIndex в ArticleController
-        'products' => 'product/list',  // actionList в ProductController
     );
