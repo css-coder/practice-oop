1c26d03c5e835180a7411aca7ff96c222f046c1e
css-coder <moskitiks@gmail.com>
2018-01-08 20:44:40 +0300

    Добавлен путь к шаблону новостей

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -9,14 +9,12 @@
             $newsList = array();
             $newsList = News::getNewsList();
 
-            echo '<pre>';
-            print_r($newsList);
-            echo '</pre>';
+            require_once (ROOT . 'views/news/index.php');
 
             return true;
         }
 
-        public function actionView($category, $id)
+        public function actionView($id)
         {
 
             if ($id) {
@@ -24,8 +22,6 @@
                 echo '<pre>';
                 print_r($newsItem);
                 echo '</pre>';
-
-                echo 'actionView';
             }
 
             return true;
